8856bf1a05ff8c25212ec45cd02ae13dbd852137
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2022-04-05 08:38:19 -0400

    queue

---
+++ src/Queue.java
@@ -0,0 +1,41 @@
+public class Queue<T> implements Iterable<T> {
+
+    private java.util.LinkedList<T> list = new java.util.LinkedList<T>();
+
+    public Queue() {
+    }
+
+    public Queue(T firstElem) {
+        offer(firstElem);
+    }
+
+    // peek element in front of the queue
+    public T peek() {
+        if (list.isEmpty()) throw new RuntimeException("Queue Empty");
+        return list.peekFirst();
+    }
+
+    // remove element in front of the queue
+    public T poll() {
+        if (isEmpty()) throw new RuntimeException("Queue Empty");
+        return list.removeFirst();
+    }
+
+    // add element to the back of the queue
+    public void offer(T elem) {
+        list.addLast(elem);
+    }
+
+    @Override
+    public java.util.Iterator<T> iterator() {
+        return list.iterator();
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+}
